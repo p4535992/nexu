84abef1049d98ccab0fa8ff5969665d77ea99650
4535992 <tentimarco0@gmail.com>
2026-07-16 13:06:24 +0200

    Add Spring Boot server configuration

---
+++ nexu-spring-boot-server/src/main/java/lu/nowina/nexu/springboot/server/NexuSpringBootConfiguration.java
@@ -0,0 +1,14 @@
+package lu.nowina.nexu.springboot.server;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import({
+        NexuHttpController.class,
+        NexuLoopbackCorsFilter.class
+})
+public class NexuSpringBootConfiguration {
+}
