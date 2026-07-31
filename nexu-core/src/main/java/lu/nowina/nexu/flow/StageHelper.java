5066dc6f658ae7034b159b6948b7c87e8fe9e61a
4535992 <tentimarco0@gmail.com>
2026-07-31 09:31:41 +0200

    Allow contextual password dialog titles

--- nexu-core/src/main/java/lu/nowina/nexu/flow/StageHelper.java
+++ nexu-core/src/main/java/lu/nowina/nexu/flow/StageHelper.java
@@ -1,72 +1,76 @@
 package lu.nowina.nexu.flow;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
+
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.apache.commons.lang.StringUtils.*;
 
 public class StageHelper {
 
-	private static StageHelper instance;
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(StageHelper.class);
+    private static StageHelper instance;
 
-	private String title;
-	
-	private ResourceBundle bundle;
-	
-	private static final String BUNDLE_NAME = "bundles/nexu";
+    private static final Logger LOGGER = LoggerFactory.getLogger(StageHelper.class);
 
-	private StageHelper() {
-	}
+    private String title;
 
-	public static synchronized StageHelper getInstance() {
-		if (instance == null) {
-			synchronized (StageHelper.class) {
-				if (instance == null) {
-					instance = new StageHelper();
-					instance.setBundle(ResourceBundle.getBundle(BUNDLE_NAME));
-				}
-			}
-		}
-		return instance;
-	}
+    private ResourceBundle bundle;
 
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(final String applicationName, final String resourceBundleKey) {
-		if(isBlank(applicationName) && isBlank(resourceBundleKey)) {
-			title = "";
-			return;
-		}
-		String translatedTitle = "";
-		try {
-			translatedTitle = getBundle().getString(resourceBundleKey);
-		} catch (MissingResourceException mre) {
-			LOGGER.warn("Resource bundle key \"{}\" is missing.", resourceBundleKey);
-		}catch(Exception e) {
-			LOGGER.error(e.getMessage(), e);
-		}
-		if(!isBlank(applicationName) && !isBlank(translatedTitle)) {
-			title = applicationName + " - " + translatedTitle;
-		} else if(isBlank(applicationName)) {
-			title = translatedTitle;
-		} else if(isBlank(translatedTitle)) {
-			title = applicationName;
-		} else {
-			title = "";
-		}
-	}
+    private static final String BUNDLE_NAME = "bundles/nexu";
 
-	public ResourceBundle getBundle() {
-		return bundle;
-	}
+    private StageHelper() {
+    }
 
-	public void setBundle(ResourceBundle bundle) {
-		this.bundle = bundle;
-	}
+    public static synchronized StageHelper getInstance() {
+        if (instance == null) {
+            synchronized (StageHelper.class) {
+                if (instance == null) {
+                    instance = new StageHelper();
+                    instance.setBundle(ResourceBundle.getBundle(BUNDLE_NAME));
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String applicationName, final String resourceBundleKey) {
+        if (isBlank(applicationName) && isBlank(resourceBundleKey)) {
+            title = "";
+            return;
+        }
+        String translatedTitle = "";
+        try {
+            translatedTitle = getBundle().getString(resourceBundleKey);
+        } catch (MissingResourceException exception) {
+            LOGGER.warn("Resource bundle key \"{}\" is missing.", resourceBundleKey);
+        } catch (Exception exception) {
+            LOGGER.error(exception.getMessage(), exception);
+        }
+        setLiteralTitle(applicationName, translatedTitle);
+    }
+
+    /** Sets a title that has already been localized by a feature-specific bundle. */
+    public void setLiteralTitle(final String applicationName, final String translatedTitle) {
+        if (!isBlank(applicationName) && !isBlank(translatedTitle)) {
+            title = applicationName + " - " + translatedTitle;
+        } else if (isBlank(applicationName)) {
+            title = isBlank(translatedTitle) ? "" : translatedTitle;
+        } else {
+            title = applicationName;
+        }
+    }
+
+    public ResourceBundle getBundle() {
+        return bundle;
+    }
+
+    public void setBundle(final ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
 }
